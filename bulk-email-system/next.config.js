6c75403a512b61b8d0a3859afb646974ff08f50c
Aakash Singh <aakash@singhjitech.com>
2026-01-01 18:47:05 +0530

    asd

--- bulk-email-system/next.config.js
+++ bulk-email-system/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   // Server Actions are enabled by default in Next.js 14
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
